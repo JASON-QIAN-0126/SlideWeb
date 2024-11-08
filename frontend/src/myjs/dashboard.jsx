49d879be0fdfd82be78f169b7cdf44f00cd480e1
My mac <qianjianghao@qianjianghaodeMacBook-Air.local>
2024-11-08 22:25:13 +1100

    update dash UI

--- frontend/src/myjs/dashboard.jsx
+++ frontend/src/myjs/dashboard.jsx
@@ -47,8 +47,9 @@ const Modal = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  background-color: #fff;
+  background-color: #d3c4f3;
   padding: 20px;
+  border-radius: 10px;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
   z-index: 1000;
   text-align: center;
